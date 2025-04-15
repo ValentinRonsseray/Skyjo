2f4e3681bbdc7361887c4c8cb727d779da81a868
Valentin <valentin.ronsseray28@gmail.com>
2025-04-15 18:05:22 +0200

    add: Draw Pile class

---
+++ Skyjo_bis/draw_pile.h
@@ -0,0 +1,11 @@
+#pragma once  
+#include "pile.h"  
+class DrawPile :  
+   public Pile  
+{  
+public:
+   DrawPile() : Pile(nullptr, {}) {}
+   DrawPile(Game* game, std::vector<Card*> cards) : Pile(game, cards) {}
+
+   inline std::vector<Card*> get_cards() const { return m_cards; }
+};
