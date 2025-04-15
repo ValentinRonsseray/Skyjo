f85d67f30caa90d4f1455fc6c3d7ecf4d5652fca
Valentin <valentin.ronsseray28@gmail.com>
2025-04-15 18:04:40 +0200

    add: Pile class

---
+++ Skyjo_bis/pile.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <vector>
+#include "card.h"
+
+class Game;
+
+class Pile
+{
+protected:
+	Game* m_game;
+	std::vector<Card*> m_cards;
+public:
+	Pile() : m_game(nullptr), m_cards({}) {}
+	Pile(Game* game, std::vector<Card*> cards) : m_game(game), m_cards(cards) {}
+};
+
