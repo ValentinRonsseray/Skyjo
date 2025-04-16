41cbeb6d484a5705f6f6dd7a902c5051d322e61f
Valentin <valentin.ronsseray28@gmail.com>
2025-04-16 20:45:44 +0200

    feat: Player class added to game

--- Skyjo_bis/game.h
+++ Skyjo_bis/game.h
@@ -3,6 +3,7 @@
 #include <map>
 #include <vector>
 #include "card.h"
+#include "player.h"
 #include "draw_pile.h"
 #include "discard_pile.h"
 #include "constants.h"
@@ -13,23 +14,22 @@ class Application;
 class Game
 {
 public:
-	Game(Application& application, unsigned int player_amount) : m_application(application), m_player_amount(player_amount)
+	Game(Application& application, unsigned int player_amount) : m_application(application), m_player_amount(player_amount), m_players(player_amount, Player()), m_discard_pile(this, {})
 	{
 		#ifdef GAME_DEBUG
 			std::cout << "Game created at address: " << this << std::endl;
 		#endif // GAME_DEBUG
 
 		m_cards = build_cards_vector();
-		
+
 		std::vector<Card*> cards_ptrs;
 		cards_ptrs.reserve(Constants::Card::CARD_NUMBER);
 		for (auto& card : m_cards)
 		{
 			cards_ptrs.push_back(&card);
 		}
-		m_draw_pile = DrawPile(this, cards_ptrs);
-		m_discard_pile = DiscardPile(this, {});
 
+		m_draw_pile = DrawPile(this, cards_ptrs);
 	};
 	~Game()
 	{
@@ -48,6 +48,8 @@ public:
 private:
 	Application& m_application;
 	unsigned int m_player_amount;
+
+	std::vector<Player> m_players;
 
 	std::vector<Card> m_cards;
 	std::vector<Card> build_cards_vector();
--- Skyjo_bis/player.h
+++ Skyjo_bis/player.h
@@ -1,5 +1,10 @@
 #pragma once
+#include "deck.h"
+
 class Player
 {
+private:
+	Deck m_deck;
+public:
+	Player() = default;
 };
-
