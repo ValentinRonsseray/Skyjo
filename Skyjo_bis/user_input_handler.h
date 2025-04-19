16572de806e4f5f9143e8e42813044e0f809b7ff
Valentin <valentin.ronsseray28@gmail.com>
2025-04-19 16:17:09 +0200

    feat(UserInputHandler)

---
+++ Skyjo_bis/user_input_handler.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "deck.h"
+
+class UserInputHandler
+{
+public:
+
+	enum class DrawCardSourceEnum
+	{
+		DRAW_PILE,
+		DISCARD_PILE
+	};
+
+	enum class CardDecisionEnum
+	{
+		REPLACE,
+		DISCARD,
+	};
+
+	UserInputHandler() = default;
+
+	/**
+	 * @brief Gets the index of the card to replace.
+	 * @return The index of the card to replace. (0 <= index <= 11)
+	 */
+	size_t choose_card_to_replace() const;
+
+	/**
+	 * @brief Gets the index of the card to reveal.
+	 * @return The index of the card to reveal. (0 <= index <= 11)
+	 */
+	size_t choose_card_to_reveal() const;
+	
+	DrawCardSourceEnum choose_where_to_draw() const;
+
+	CardDecisionEnum choose_discard_or_replace() const;
+};
+
