9e96700d84ab6a50c61fb9fc65cd6b91a657a81a
Valentin <valentin.ronsseray28@gmail.com>
2025-07-10 22:53:44 +0200

    feat(Utils): integer random pickers

--- Skyjo_bis/utils.h
+++ Skyjo_bis/utils.h
@@ -31,4 +31,30 @@ namespace Utils
         size_type index = dis(gen);
         return elements[index];
     }
+
+	/**
+	 * @brief Returns a random integer between min and max (inclusive).
+	 * @param min The minimum value (inclusive).
+	 * @param max The maximum value (inclusive).
+	 * @return A random integer between min and max.
+	 */
+	int random_integer(int min, int max)
+	{
+		if (min > max)
+			throw std::invalid_argument("Min cannot be greater than max.");
+		if (min == max)
+			return min;
+		std::random_device rd;
+		std::mt19937 gen(rd());
+		std::uniform_int_distribution<int> dis(min, max);
+		return dis(gen);
+	}
+
+	int random_integer(int max)
+	{
+		if (max < 0)
+			throw std::invalid_argument("Max cannot be negative.");
+		return random_integer(0, max);
+	}
+
 }
